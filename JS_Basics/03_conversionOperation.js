7caf2636b6c4a0f3915b78daa68461d8bcfe4c80
prince raj <100460531+Prince8412@users.noreply.github.com>
2023-08-09 18:34:02 +0000

    conversion of datatypes

---
+++ JS_Basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// ////string
+// ////string
+// ////number
+// ////NaN
+
+// these four lines will be the output of the line 1 to 9
+
+let score = null;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// object
+// object
+// number
+// 0
+// If score will be undefined then output of line number 25 will be NaN
+// If score will be boolean then output of line number 25 will be '0' or '1'
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1; and false = 0 we have
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); // here output will be true
+
+// we can say that for isLoggedIn = 0 o/p => true and for isLoggedIn = 0 o/p => false
+// for blank value output will be false;
+// and for some name or any other value it will give the output true;
+
+// we can convert the datatypes into (string, boolean, Number, etc).
+
+// Following is the given example for the String
+
+let someNumber = 33;
+console.log(typeof (someNumber));
+
+let StringSomeNumber = String(someNumber);
+console.log(typeof StringSomeNumber);
+console.log(StringSomeNumber);
