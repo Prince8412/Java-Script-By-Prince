e66b5ab70802d86291d227f86a03708efdc242c3
prince raj <100460531+Prince8412@users.noreply.github.com>
2023-08-10 18:20:32 +0000

    Comparision in JavaScript

---
+++ JS_Basics/05_Comparision.js
@@ -0,0 +1,24 @@
+console.log(2>1); // output = true
+console.log(2>=1); // output = true
+console.log(2!=1);
+console.log(2===1);
+console.log(2<=1);
+console.log(2<1);
+
+
+console.log("2">1);
+console.log("02"==1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+/*=== means it will check the value strictly
+and == will check without any strictness means it will not check the datatypes of the data*/
+
+
+console.log("2" === 2);
