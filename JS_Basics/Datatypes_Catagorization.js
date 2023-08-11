8a2369ef461048209bd64724fa0b34be6fb1de52
prince raj <100460531+Prince8412@users.noreply.github.com>
2023-08-11 17:33:38 +0000

    DataType Catagorization in Summary

---
+++ JS_Basics/Datatypes_Catagorization.js
@@ -0,0 +1,38 @@
+// PRIMITIVE Datatypes
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// header is onabort
+
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null;
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // output = false;
+
+const bigNumber = 23456432134567n // here n represents the BigInt
+
+// Referance (Non Primitive)
+// Array, Objects, Function
+
+const Heros = ["Shatiman", "Naagraj", "Hanuman Ji"] // This line is an example of Array
+
+let MyObject = { // In Curly Brackets We write Objects
+    name: "Prince",
+    age: 22,
+}
+
+const MyFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber); // Output will be "BigInt"
+console.log(typeof outsideTemp); // Output will be "object"
+console.log(typeof score); // Output will be "number"
+console.log(typeof scoreValue); // Output will be "number"
