32a11c4804729dcda099920cd51b4647c697980c
prince raj <100460531+Prince8412@users.noreply.github.com>
2023-10-13 07:11:50 +0000

    INRtoDoller

--- AlltypeFunctions/inrToDoller.js
+++ AlltypeFunctions/inrToDoller.js
@@ -2,7 +2,7 @@ const transactions = [1000, 2000, 3000, 400, -880, -800];
 const inrtoDoller = 80;
 
 const InDoller = transactions.map((amount)=>{
-    return amount/inrtoDoller;
+    return amount/inrtoDoller.toFixed(0);  // Here toFixed(0) is used to convert all the float values in integer
 })
 
 console.log(InDoller);
